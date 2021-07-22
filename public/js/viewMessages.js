0f45dbdcdc3bd7e69c4401be9099c2ea3d0c1f67
KimchiOverRice <ksattary123@gmail.com>
2021-07-22 16:55:09 +0000

    can read passcode and data from db

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,4 +1,14 @@
 
 const getMessages = () => {
-    console.log("Get messages")
+    const passcode = document.querySelector("#passcode")
+    console.log("button pressed", passcode.value)
+
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val()
+
+        for(let key in data){
+            console.log(key, data[key])
+        }
+    })
 }
