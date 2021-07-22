7c75b5e901c8a870109ad09802a28f5192c33f26
KimchiOverRice <ksattary123@gmail.com>
2021-07-22 17:07:48 +0000

    viewing messages working

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -9,8 +9,11 @@ const getMessages = () => {
         const data = snapshot.val()
 
         for(let key in data){
-            if(passcode === passcode.value){
+            console.log(key, data[key])
+            if(key === passcode.value){
                 console.log("match found")
+                const message = document.querySelector("#message")
+                message.innerHTML = data[key]
             }
         }
     })
